8e937afdeacdcc88f75cab3cd875752a5d18a1dd
Savcdilara <108372749+Savcdilara@users.noreply.github.com>
2023-09-25 16:49:06 +0000

    push file

---
+++ SpotifyPlayList/Entities/Playlist.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpotifyPlayList.Entities
+{
+    internal class Playlist
+    {
+        public Guid Id { get; set; }
+        private List<Song> Songs { get; set; }
+
+        private Random random;
+        public Playlist(Song firstSong)
+        {
+            Songs = new List<Song>();
+            random = new();
+            AddSong(firstSong);
+        }
+
+        public void AddSong(Song song)
+        {
+            if (song != null)
+            {
+                Songs.Add(song);
+            }
+        }
+
+        public string GetSongs()
+        {
+            return string.Join("\n ", Songs.Select(song => $"{song.Title} - {song.Composer}"));
+        }
+
+        public void ShuffleSongs()
+        {
+            int count = Songs.Count;
+
+            while (count > 0)
+            {
+                count--;
+
+                Song song = Songs[count];
+
+                int randomIndex = random.Next(count);
+
+                Songs[count] = Songs[randomIndex];
+
+                Songs[randomIndex] = song;
+
+            }
+
+        }
+    }
+}
---
+++ SpotifyPlayList/Program.cs
@@ -0,0 +1,22 @@
+﻿using SpotifyPlayList.Entities;
+
+Console.WriteLine("Spotify - Dilara's musics...");
+
+var song1 = new Song("Dernière Danse", " Indila", "Adila Sedraïa");
+var song2 = new Song("Tourner Dans Le Vide", " Indila", "Adila Sedraïa");
+var song3 = new Song("Mockingbird", "Eminem", "Luis Resto");
+
+var playlist1 = new Playlist(song1);
+playlist1.AddSong(song2);
+playlist1.AddSong(song3);
+
+Console.WriteLine("\n\nBefore Shuffle:");
+Console.WriteLine(playlist1.GetSongs());
+
+playlist1.ShuffleSongs();
+
+Console.WriteLine("\n\nAfter Shuffle:");
+Console.WriteLine(playlist1.GetSongs());
+
+
+
